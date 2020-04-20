07e12162df77e884db0f60fc5c5bcd86816968d0
Jonathan Hsu <jonhsu.design@gmail.com>
2020-04-20 13:03:13 -0700

    finished mvp and all stretch goals

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
+import MovieCard from './MovieCard';
 
 const MovieList = props => {
   const [movies, setMovies] = useState([])
@@ -9,6 +11,7 @@ const MovieList = props => {
         .get('http://localhost:5000/api/movies')
         .then(response => {
           setMovies(response.data);
+          console.log(response.data);
         })
         .catch(error => {
           console.error('Server Error', error);
@@ -20,33 +23,37 @@ const MovieList = props => {
   
   return (
     <div className="movie-list">
-      {movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
+      {movies.map((movie, index) => (
+        <Link key={index} to={`/movies/${movie.id}`}>
+          <MovieCard key={movie.id} movie={movie} />
+        </Link>
       ))}
     </div>
   );
 }
 
-function MovieDetails({ movie }) {
-  const { title, director, metascore, stars } = movie;
-  return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
+// function MovieDetails({ movie }) {
+//   const { id, title, director, metascore, stars } = movie;
+//   return (
+//     <Link to={`/movies/${id}`}>
+//       <div className="movie-card">
+//         <h2>{title}</h2>
+//         <div className="movie-director">
+//           Director: <em>{director}</em>
+//         </div>
+//         <div className="movie-metascore">
+//           Metascore: <strong>{metascore}</strong>
+//         </div>
+//         <h3>Actors</h3>
 
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
-        </div>
-      ))}
-    </div>
-  );
-}
+//         {stars.map(star => (
+//           <div key={star} className="movie-star">
+//             {star}
+//           </div>
+//         ))}
+//       </div>
+//     </Link>
+//   );
+// }
 
 export default MovieList;
--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { useHistory, NavLink } from 'react-router-dom'
 
-const SavedList = props => (
-  <div className="saved-list">
-    <h3>Saved Movies:</h3>
-    {props.list.map(movie => (
-      <span className="saved-movie">{movie.title}</span>
-    ))}
-    <div className="home-button">Home</div>
-  </div>
-);
+const SavedList = props => {
+  const history = useHistory();
+
+  const goHome = event => {
+    history.push('/');
+  }
+
+  return (
+    <div className="saved-list">
+      <h3>Saved Movies:</h3>
+      {props.list.map((movie, index) => (
+        <NavLink key ={index} to={`/movies/${movie.id}`}><span className="saved-movie">{movie.title}</span></NavLink>
+      ))}
+        <div onClick={goHome} className="home-button">Home</div>
+    </div>
+  )
+};
 
 export default SavedList;
